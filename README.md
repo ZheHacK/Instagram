62f2aa29d36c6f98c99c6d0da2f886fc20b9ac68
ZheHacK <dhenhabsyi55@gmail.com>
2019-09-15 01:11:40 +0700

    /data/data/com.termux/files/home/Instagram

---
+++ README.md
@@ -0,0 +1,36 @@
+## INSTAGRAM TOOLS [TOOLSIG]
+* Instagram Tools Based on NodeJS (This script or code was created by ccocot - ZheAlhaqy).
+* Tools (BomLikeTarget,Botlike1,Botlike2,Dellallphoto,Fah,Fftauto,Flaauto,Flmauto,Unfollall,Unfollnotfollback).
+
+## Tutorial on Termux [ANDROID]
+	$ pkg install git
+	$ pkg install nodejs-lts
+	$ git clone https://github.com/auliaahmad165/IG && cd IG
+	$ unzip node_modules.zip
+	$ node index.js
+
+## Tutorial on C9io/CodeAnywhere [WEB VERSION]
+	$ Login c9.io/login | https://codeanywhere.com/login
+	$ Select & Install Workspace + Package NodeJS
+	$ nvm install 10.7.0 && nvm use 10.7.0 && nvm alias default 10.7.0
+	$ git clone https://github.com/auliaahmad165/IG && cd IG
+	$ unzip node_modules.zip
+	$ node index.js
+
+## Tutorial on PC/LAPTOP [OS]
+	# Download & Install Git for Windows (Link Download: https://s.id/2yGtu) *direct_link
+	# Download & Install NodeJs for Windows (Versi 9.3.0)
+	  -> 64bit (https://nodejs.org/dist/v9.3.0/node-v9.3.0-x64.msi) *direct_link_64bit
+	  -> 32bit (https://nodejs.org/dist/v9.3.0/node-v9.3.0-x86.msi) *direct_link_32bit
+	# Download File toolsig (https://github.com/auliaahmad165/IG) & Extract the file
+	# Right Click (Mouse/Touchpad) In toolsig-master Folder & Choose Git Bash Here!
+	$ unzip node_modules.zip
+	$ node index.js
+
+## My Social Media
+	☑ Instagram	: https://www.instagram.com/zhe_pengumbara
+	☑ Facebook	: https://www.facebook.com/dhenhabsyi
+	☑ Github	: https://github.com/ZheHacK
+	
+## License
+	☑ This project is licensed under MIT License 2017-2019. https://opensource.org/licenses/MIT.
---
+++ index.js
@@ -0,0 +1,101 @@
+﻿'use strict'
+
+// Recode by @zhe_pengumbara
+// Last modified by ZheAlHaqy (@zhe_pengumbara) on September 14, 2019
+// ig : @zhe_pengumbara
+
+const Client = require('instagram-private-api').V1; 
+const delay = require('delay');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+var moment = require("moment"); //DETECT CALENDER
+var colors = require('colors'); //DETECT COLORS
+var userHome = require('user-home'); //DETECT USER PC/HOME
+
+//DETECT IP *START!
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+//DETECT IP *END!
+
+const questionTools = [
+{
+  type:"list",
+  name:"Tools",
+  message:"Select tools:\n ",
+  choices:
+  [
+  "► Bom Like Target              [USING ITTYW/DELAY]",
+  "► Mass Delete Post/Photo       [USING ITTYW/DELAY]",
+  "► Unfollow All Following       [USING ITTYW/DELAY]",
+  "► Unfollow Not Followback      [USING ITTYW/DELAY]",
+  "► Follow Followers Target      [USING ITTYW/DELAY]",
+  "\n"
+  ] 
+}
+]
+const main = async () => {
+  //Last modified by ZheAlhaqy (@zhe_pengumbara) on September 14, 2019
+  try{
+    var toolChoise = await inquirer.prompt(questionTools);
+    toolChoise = toolChoise.Tools;
+    switch(toolChoise){
+
+      case "► Bom Like Target              [USING ITTYW/DELAY]":
+      const bomliketarget = require('./tools/bomliketarget.js');
+      await bomliketarget();
+      break;
+
+      case "► Mass Delete Post/Photo       [USING ITTYW/DELAY]":
+      const dellallphoto = require('./tools/dellallphoto.js');
+      await dellallphoto();
+      break;
+
+      case "► Unfollow All Following       [USING ITTYW/DELAY]":
+      const unfollall = require('./tools/unfollall.js');
+      await unfollall();
+      break;
+
+      case "► Unfollow Not Followback      [USING ITTYW/DELAY]":
+      const unfollnotfollback = require('./tools/unfollnotfollback.js');
+      await unfollnotfollback();
+      break;
+
+      case "► Follow Followers Target      [USING ITTYW/DELAY]":
+      const fftauto = require('./tools/fftauto.js');
+      await fftauto();
+      break;
+
+      default:
+      console.log("\n ERROR:".red.bold,"Aw, Snap! Something went wrong while displaying this tool!\n".green.bold,"NOT FOUND! Please try again!".yellow.bold);
+    }
+  } catch(e) {
+    }
+  }
+  //Last modified by Zhe AlHaqy (@zhe_pengumbara) on September 14, 2019
+
+  console.log(chalk`{bold.green
+  Ξ TITLE  : INSTAGRAM PRIVATE TOOLS
+  Ξ UPLOAD : 29/08/2019 [16.28 WIB]
+  Ξ CODEBY : Zhe AlHaqy [Ccocot Ccocot]
+  Ξ UPDATE : zhe_pengumbara [@zhe_pengumbara]
+  }`);
+  console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
+  console.log("  Ξ START  : ".bold.red + moment().format('D MMMM YYYY, h:mm:ss a'));
+  console.log("  Ξ YPATH  : ".bold.red +userHome);
+  console.log("  Ξ YOUIP  : ".bold.red +addresses);
+  console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
+  console.log(chalk`{bold.yellow
+  Ξ THANKS : Instagram Private Tools [Original Source File]
+           : Zhe AlHaqy | zhe_pengumbara
+           : Muslim Cyber Army}`);
+  console.log('\n')
+  main();
